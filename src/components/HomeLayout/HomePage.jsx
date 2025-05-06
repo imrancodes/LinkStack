d1c619cd389df864d5983758b7dbbfbe58ef8966
KHAN IMRAN <imrankhanshakeel012@gmail.com>
2025-05-06 05:49:46 +0530

    update HomePage.jsx and link.jsx almost completed the link page

--- src/components/HomeLayout/HomePage.jsx
+++ src/components/HomeLayout/HomePage.jsx
@@ -1,41 +1,113 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Header';
 import MobilePreview from './MobilePreview';
 import Button from '../CommonComponents/Button';
 import Item from '../../assets/item2.png';
 import Links from './Links';
 
-
 const HomePage = () => {
-  return (
-    <>
-      <Header />
-      <div className='flex gap-8 '>
-        <MobilePreview />
-        <div className='bg-[#191919d4] w-[70%] rounded-lg mr-4'>
-          <div className='px-8 py-4'>
-            <h1 className='text-3xl font-semibold mb-3'>Customize your links</h1>
-            <p className='text-lg mb-8'>Add/edit/remove links below and then share all your profiles with the world!</p>
-            <Button className='w-full border border-[#643CFF] text-[#643CFF] py-3 rounded-lg hover:bg-[#633cff26]'>+ Add a new Link</Button>
-          </div>
-          <div className='max-h-[430px] mx-8 my-4 links flex flex-col gap-4 overflow-y-auto scrollbar-hide'>
-            <Links/>
-            <Links/>
-            <Links/>
-          </div>
-          <div className='bg-black rounded-lg p-4 mx-8 my-4 flex flex-col items-center justify-center hidden'>
-            <img src={Item} alt="" className='w-[250px] h-[161px] mt-4' />
-            <h1 className='text-3xl mt-8 mb-6'>Let's get you started</h1>
-            <p className='text-md pb-20 text-center'>Click the button above to add your first link. <br /> You can edit or remove it anytime.</p>
-          </div>
-          <div className='bg-black h-1' />
-          <div className='flex justify-end'>
-            <Button className='bg-[#643CFF] px-6 py-2 mt-6 mr-8 hover:bg-[#633cffe2]'>Save</Button>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+    const [showComponent, setShowComponent] = useState(false);
+    const [links, setLinks] = useState([]);
+
+    const handleShowLinks = () => {
+        const newLink = {
+            id: Date.now(),
+            platform: '',
+            url: '',
+            icon: '',
+        };
+        setLinks((prev) => [...prev, newLink]);
+        setShowComponent(true);
+    };
+
+    const handleRemoveLink = (id) => {
+        const updatedLinks = links.filter((link) => link.id !== id);
+        setLinks(updatedLinks);
+
+        if (updatedLinks.length === 0) {
+            setShowComponent(false);
+        }
+    };
+
+    const handleUpdateLink = (id, field, value) => {
+        const updated = links.map((link) => {
+            return link.id === id ? { ...link, [field]: value } : link;
+        });
+        setLinks(updated);
+    };
+
+    const handleDataSave = () => {
+      console.log(links);
+    };
+
+    return (
+        <>
+            <Header />
+            <div className="flex gap-8 ">
+                <MobilePreview />
+                <div className="bg-[#191919d4] w-[70%] rounded-lg mr-4">
+                    <div className="px-8 py-4">
+                        <h1 className="text-3xl font-semibold mb-3">
+                            Customize your links
+                        </h1>
+                        <p className="text-lg mb-8">
+                            Add/edit/remove links below and then share all your
+                            profiles with the world!
+                        </p>
+                        <Button
+                            className="w-full border border-[#643CFF] text-[#643CFF] py-3 rounded-lg hover:bg-[#633cff26]"
+                            onClick={handleShowLinks}>
+                            + Add a new Link
+                        </Button>
+                    </div>
+                    <div
+                        className={`min-h-[430px] mx-8 my-4 links flex flex-col gap-4 overflow-y-auto scrollbar-hide ${
+                            showComponent ? 'h-[430px]' : 'h-0'
+                        } ${showComponent ? 'block' : 'hidden'} `}>
+                        {links.map((link, i) => (
+                            <Links
+                                order={i + 1}
+                                key={link.id}
+                                id={link.id}
+                                removeLink={handleRemoveLink}
+                                updateLink={handleUpdateLink}
+                                platform={link.platform}
+                                url={link.url}
+                                
+                            />
+                        ))}
+                    </div>
+                    <div
+                        className={`bg-black rounded-lg p-4 mx-8 my-4 flex flex-col items-center justify-center ${
+                            showComponent ? 'hidden' : 'block'
+                        }`}>
+                        <img
+                            src={Item}
+                            alt=""
+                            className="w-[250px] h-[161px] mt-4"
+                        />
+                        <h1 className="text-3xl mt-8 mb-6">
+                            Let's get you started
+                        </h1>
+                        <p className="text-md pb-20 text-center">
+                            Click the button above to add your first link.{' '}
+                            <br /> You can edit or remove it anytime.
+                        </p>
+                    </div>
+                    <div className="bg-black h-1" />
+                    <div className="flex justify-end">
+                        <Button
+                            onClick={handleDataSave}
+                            className={`bg-[#643CFF] px-6 py-2 mt-6 mr-8 disabled:bg-[#977ff8] disabled:cursor-not-allowed hover:bg-[#633cffe2]`}
+                            type="submit"
+                            disabled={links.length === 0}>
+                            Save
+                        </Button>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default HomePage;
--- src/components/HomeLayout/Links.jsx
+++ src/components/HomeLayout/Links.jsx
@@ -3,12 +3,12 @@ import { FaLink, FaStackOverflow } from 'react-icons/fa';
 import { CgArrowLongRight } from 'react-icons/cg';
 import Button from '../CommonComponents/Button';
 import Input from '../CommonComponents/Input';
-import { FaInstagram } from "react-icons/fa";
-import { FaSnapchat } from "react-icons/fa6";
-import { FaTiktok } from "react-icons/fa6";
-import { FaWhatsapp } from "react-icons/fa";
-import { FaPinterestP } from "react-icons/fa";
-import { TbBrandOnlyfans } from "react-icons/tb";
+import { FaInstagram } from 'react-icons/fa';
+import { FaSnapchat } from 'react-icons/fa6';
+import { FaTiktok } from 'react-icons/fa6';
+import { FaWhatsapp } from 'react-icons/fa';
+import { FaPinterestP } from 'react-icons/fa';
+import { TbBrandOnlyfans } from 'react-icons/tb';
 import {
   Github,
   Linkedin,
@@ -22,15 +22,23 @@ import {
   Send,
 } from 'lucide-react';
 
-const Links = () => {
+const Links = ({ order, removeLink, id, updateLink, platform, url }) => {
   const linkOptions = [
     { name: 'GitHub', value: 'github', icon: <Github size={18} /> },
     { name: 'LinkedIn', value: 'linkedin', icon: <Linkedin size={18} /> },
     { name: 'Portfolio', value: 'portfolio', icon: <Globe size={18} /> },
     { name: 'Dev.to', value: 'devto', icon: <Code2 size={18} /> },
-    { name: 'Stack Overflow', value: 'stackoverflow', icon: <FaStackOverflow size={18} /> },
+    {
+      name: 'Stack Overflow',
+      value: 'stackoverflow',
+      icon: <FaStackOverflow size={18} />,
+    },
     { name: 'Hashnode', value: 'hashnode', icon: <Code size={18} /> },
-    { name: 'Instagram', value: 'instagram', icon: <FaInstagram size={18} /> },
+    {
+      name: 'Instagram',
+      value: 'instagram',
+      icon: <FaInstagram size={18} />,
+    },
     { name: 'Twitter (X)', value: 'twitter', icon: <Twitter size={18} /> },
     { name: 'Facebook', value: 'facebook', icon: <Facebook size={18} /> },
     { name: 'YouTube', value: 'youtube', icon: <Youtube size={18} /> },
@@ -38,65 +46,83 @@ const Links = () => {
     { name: 'TikTok', value: 'tiktok', icon: <FaTiktok size={18} /> },
     { name: 'WhatsApp', value: 'whatsapp', icon: <FaWhatsapp size={18} /> },
     { name: 'Telegram', value: 'telegram', icon: <Send size={18} /> },
-    { name: 'Pinterest', value: 'pinterest', icon: <FaPinterestP size={18} /> },
-    { name: 'Only Fans', value: 'onlyfans', icon: <TbBrandOnlyfans size={18} /> },
+    {
+      name: 'Pinterest',
+      value: 'pinterest',
+      icon: <FaPinterestP size={18} />,
+    },
+    {
+      name: 'Only Fans',
+      value: 'onlyfans',
+      icon: <TbBrandOnlyfans size={18} />,
+    },
   ];
 
   const [selected, setSelected] = useState('');
   const [isOpen, setIsOpen] = useState(false);
 
-  const selectedOption = linkOptions.find(opt => opt.value === selected);
+  const selectedOption = linkOptions.find((opt) => opt.value === selected);
 
   return (
     <div className="bg-black rounded-lg p-4">
       <div className="flex justify-between items-center text-gray-200 mb-4">
         <div className="flex gap-1 items-center">
           <CgArrowLongRight />
-          <h1>Link #1</h1>
+          <h1>Link #{order}</h1>
         </div>
-        <Button>Remove</Button>
+        <Button onClick={() => removeLink(id)}>Remove</Button>
       </div>
 
-      {/* Custom Dropdown */}
       <div className="relative mx-auto mt-5">
-        <label className="block text-sm mb-2 text-white">Platform</label>
+        <label className="block text-sm mb-2 text-white">
+          Platform
+        </label>
 
         <div
           className="w-full border border-gray-500 p-2 rounded bg-black text-white cursor-pointer flex justify-between items-center"
-          onClick={() => setIsOpen(!isOpen)}
-        >
+          onClick={() => setIsOpen(!isOpen)}>
           {selectedOption ? (
-            <div className="flex gap-2 items-center">{selectedOption.icon}<span>{selectedOption.name}</span></div>
+            <div className="flex gap-2 items-center">
+              {selectedOption.icon}
+              <span>{selectedOption.name}</span>
+            </div>
           ) : (
             <span className="text-gray-400">Choose a platform</span>
           )}
           <span className="ml-auto">&#9662;</span>
         </div>
+        {/* {!selected && (
+          <p className="text-red-500 text-sm px-4">
+            Please select a platform
+          </p>
+        )} */}
 
         {isOpen && (
           <div className="absolute z-10 w-full bg-black border border-gray-600 mt-1 rounded shadow-lg max-h-60 overflow-y-auto">
-            {linkOptions.map(option => (
-              <div
-                key={option.value}
-                className="flex items-center gap-2 px-4 py-2 hover:bg-gray-700 cursor-pointer"
-                onClick={() => {
-                  setSelected(option.value);
-                  setIsOpen(false);
-                }}
-              >
-                {option.icon}
-                {option.name}
+            {linkOptions.map((option) => (
+              <div key={option.value || 'no-value'}>
+                <div
+                  className="flex items-center gap-2 px-4 py-2 hover:bg-gray-700 cursor-pointer"
+                  onClick={() => {
+                    if (!option.value) return;
+                    setSelected(option.value);
+                    updateLink(id, platform, option.value);
+                    setIsOpen(false);
+                  }}>
+                  {option.icon}
+                  {option.name}
+                </div>
               </div>
             ))}
           </div>
         )}
 
-        {/* Input Field */}
         <div className="relative mt-4">
           <Input
             label={'Link'}
             classname="pl-7.5"
             placeholder={'e.g. https://yourlink.com'}
+            onChange={(e) => updateLink(id, url, e.target.value)}
           />
           <FaLink className="absolute top-[42px] left-[7px]" />
         </div>
