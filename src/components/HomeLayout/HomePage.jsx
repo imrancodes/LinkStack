2721c82455dbfcf1f2c81a16aa81f093b9438414
KHAN IMRAN <imrankhanshakeel012@gmail.com>
2025-05-07 19:28:34 +0530

    minor changes

--- src/components/HomeLayout/HomePage.jsx
+++ src/components/HomeLayout/HomePage.jsx
@@ -4,12 +4,17 @@ import MobilePreview from './MobilePreview';
 import Button from '../CommonComponents/Button';
 import Item from '../../assets/item2.png';
 import Links from './Links';
+import { getFirestore, collection, addDoc } from 'firebase/firestore';
+import { app } from '../../firebase.js';
+
+const db = getFirestore(app);
 
 const HomePage = () => {
     const [showComponent, setShowComponent] = useState(false);
     const [links, setLinks] = useState([]);
+    const [errors, setErrors] = useState({ platform: false, url: false });
 
-    const handleShowLinks = () => {
+    const handleShowLinks = async () => {
         const newLink = {
             id: Date.now(),
             platform: '',
@@ -29,15 +34,59 @@ const HomePage = () => {
         }
     };
 
-    const handleUpdateLink = (id, field, value) => {
-        const updated = links.map((link) => {
-            return link.id === id ? { ...link, [field]: value } : link;
-        });
-        setLinks(updated);
+    const handleUpdateLink = (id, field, value, icon) => {
+        setLinks((prevLinks) =>
+            prevLinks.map((link) => {
+                if (link.id === id) {
+                    if (field === 'platform') {
+                        return {
+                            ...link,
+                            platform: value,
+                            icon: icon,
+                        };
+                    }
+                    return { ...link, [field]: value };
+                }
+                return link;
+            })
+        );
     };
 
-    const handleDataSave = () => {
-      console.log(links);
+    const handleDataSave = async () => {
+        const newErrors = {};
+        const urlPattern = /^(ftp|http|https):\/\/[^ "]+$/;
+
+        links.forEach((link) => {
+            if (!link.platform) newErrors[link.id] = { platform: true };
+            if (!link.url) {
+                newErrors[link.id] = {
+                    ...newErrors[link.id],
+                    url: true,
+                    message: 'URL is required',
+                };
+            } else if (!urlPattern.test(link.url)) {
+                newErrors[link.id] = {
+                    ...newErrors[link.id],
+                    url: true,
+                    message: 'Invalid URL format',
+                };
+            }
+        });
+
+        setErrors(newErrors);
+
+        if (Object.keys(newErrors).length === 0) {
+            try {
+                for (const link of links) {
+                    const docRef = await addDoc(collection(db, 'links'), link);
+                    console.log(`Document written with ID: ${docRef.id} for link:`, link);
+                }
+            } catch (e) {
+                console.error('Error adding document: ', e);
+            }
+            console.log('Links:', links);
+        }
+        
     };
 
     return (
@@ -73,7 +122,7 @@ const HomePage = () => {
                                 updateLink={handleUpdateLink}
                                 platform={link.platform}
                                 url={link.url}
-                                
+                                errors={errors[link.id]}
                             />
                         ))}
                     </div>
--- src/components/HomeLayout/Links.jsx
+++ src/components/HomeLayout/Links.jsx
@@ -9,126 +9,158 @@ import { FaTiktok } from 'react-icons/fa6';
 import { FaWhatsapp } from 'react-icons/fa';
 import { FaPinterestP } from 'react-icons/fa';
 import { TbBrandOnlyfans } from 'react-icons/tb';
-import {
-  Github,
-  Linkedin,
-  Globe,
-  Code2,
-  Code,
-  Instagram,
-  Twitter,
-  Facebook,
-  Youtube,
-  Send,
-} from 'lucide-react';
+import { FaGithub } from 'react-icons/fa';
+import { FaLinkedin } from 'react-icons/fa';
+import { FaDev } from 'react-icons/fa';
+import { IoGlobeOutline } from 'react-icons/io5';
+import { FaHashnode } from 'react-icons/fa6';
+import { FaXTwitter } from 'react-icons/fa6';
+import { FaYoutube } from 'react-icons/fa6';
+import { FaFacebook } from 'react-icons/fa';
+import { RiTelegram2Fill } from 'react-icons/ri';
 
-const Links = ({ order, removeLink, id, updateLink, platform, url }) => {
-  const linkOptions = [
-    { name: 'GitHub', value: 'github', icon: <Github size={18} /> },
-    { name: 'LinkedIn', value: 'linkedin', icon: <Linkedin size={18} /> },
-    { name: 'Portfolio', value: 'portfolio', icon: <Globe size={18} /> },
-    { name: 'Dev.to', value: 'devto', icon: <Code2 size={18} /> },
-    {
-      name: 'Stack Overflow',
-      value: 'stackoverflow',
-      icon: <FaStackOverflow size={18} />,
-    },
-    { name: 'Hashnode', value: 'hashnode', icon: <Code size={18} /> },
-    {
-      name: 'Instagram',
-      value: 'instagram',
-      icon: <FaInstagram size={18} />,
-    },
-    { name: 'Twitter (X)', value: 'twitter', icon: <Twitter size={18} /> },
-    { name: 'Facebook', value: 'facebook', icon: <Facebook size={18} /> },
-    { name: 'YouTube', value: 'youtube', icon: <Youtube size={18} /> },
-    { name: 'Snapchat', value: 'snapchat', icon: <FaSnapchat size={18} /> },
-    { name: 'TikTok', value: 'tiktok', icon: <FaTiktok size={18} /> },
-    { name: 'WhatsApp', value: 'whatsapp', icon: <FaWhatsapp size={18} /> },
-    { name: 'Telegram', value: 'telegram', icon: <Send size={18} /> },
-    {
-      name: 'Pinterest',
-      value: 'pinterest',
-      icon: <FaPinterestP size={18} />,
-    },
-    {
-      name: 'Only Fans',
-      value: 'onlyfans',
-      icon: <TbBrandOnlyfans size={18} />,
-    },
-  ];
+const Links = ({
+    order,
+    removeLink,
+    id,
+    updateLink,
+    errors,
+}) => {
+    const linkOptions = [
+        { name: 'GitHub', value: 'github', icon: <FaGithub size={18} /> },
+        { name: 'LinkedIn', value: 'linkedin', icon: <FaLinkedin size={18} /> },
+        {
+            name: 'Portfolio',
+            value: 'portfolio',
+            icon: <IoGlobeOutline size={18} />,
+        },
+        { name: 'Dev.to', value: 'devto', icon: <FaDev size={18} /> },
+        {
+            name: 'Stack Overflow',
+            value: 'stackoverflow',
+            icon: <FaStackOverflow size={18} />,
+        },
+        { name: 'Hashnode', value: 'hashnode', icon: <FaHashnode size={18} /> },
+        {
+            name: 'Instagram',
+            value: 'instagram',
+            icon: <FaInstagram size={18} />,
+        },
+        {
+            name: 'Twitter (X)',
+            value: 'twitter',
+            icon: <FaXTwitter size={18} />,
+        },
+        { name: 'Facebook', value: 'facebook', icon: <FaFacebook size={18} /> },
+        { name: 'YouTube', value: 'youtube', icon: <FaYoutube size={18} /> },
+        { name: 'Snapchat', value: 'snapchat', icon: <FaSnapchat size={18} /> },
+        { name: 'TikTok', value: 'tiktok', icon: <FaTiktok size={18} /> },
+        { name: 'WhatsApp', value: 'whatsapp', icon: <FaWhatsapp size={18} /> },
+        {
+            name: 'Telegram',
+            value: 'telegram',
+            icon: <RiTelegram2Fill size={18} />,
+        },
+        {
+            name: 'Pinterest',
+            value: 'pinterest',
+            icon: <FaPinterestP size={18} />,
+        },
+        {
+            name: 'Only Fans',
+            value: 'onlyfans',
+            icon: <TbBrandOnlyfans size={18} />,
+        },
+    ];
 
-  const [selected, setSelected] = useState('');
-  const [isOpen, setIsOpen] = useState(false);
+    const [selected, setSelected] = useState('');
+    const [isOpen, setIsOpen] = useState(false);
 
-  const selectedOption = linkOptions.find((opt) => opt.value === selected);
+    const selectedOption = linkOptions.find((opt) => opt.value === selected);
 
-  return (
-    <div className="bg-black rounded-lg p-4">
-      <div className="flex justify-between items-center text-gray-200 mb-4">
-        <div className="flex gap-1 items-center">
-          <CgArrowLongRight />
-          <h1>Link #{order}</h1>
-        </div>
-        <Button onClick={() => removeLink(id)}>Remove</Button>
-      </div>
-
-      <div className="relative mx-auto mt-5">
-        <label className="block text-sm mb-2 text-white">
-          Platform
-        </label>
-
-        <div
-          className="w-full border border-gray-500 p-2 rounded bg-black text-white cursor-pointer flex justify-between items-center"
-          onClick={() => setIsOpen(!isOpen)}>
-          {selectedOption ? (
-            <div className="flex gap-2 items-center">
-              {selectedOption.icon}
-              <span>{selectedOption.name}</span>
-            </div>
-          ) : (
-            <span className="text-gray-400">Choose a platform</span>
-          )}
-          <span className="ml-auto">&#9662;</span>
-        </div>
-        {/* {!selected && (
-          <p className="text-red-500 text-sm px-4">
-            Please select a platform
-          </p>
-        )} */}
-
-        {isOpen && (
-          <div className="absolute z-10 w-full bg-black border border-gray-600 mt-1 rounded shadow-lg max-h-60 overflow-y-auto">
-            {linkOptions.map((option) => (
-              <div key={option.value || 'no-value'}>
-                <div
-                  className="flex items-center gap-2 px-4 py-2 hover:bg-gray-700 cursor-pointer"
-                  onClick={() => {
-                    if (!option.value) return;
-                    setSelected(option.value);
-                    updateLink(id, platform, option.value);
-                    setIsOpen(false);
-                  }}>
-                  {option.icon}
-                  {option.name}
+    return (
+        <div className="bg-black rounded-lg p-4">
+            <div className="flex justify-between items-center text-gray-200 mb-4">
+                <div className="flex gap-1 items-center">
+                    <CgArrowLongRight />
+                    <h1>Link #{order}</h1>
                 </div>
-              </div>
-            ))}
-          </div>
-        )}
+                <Button onClick={() => removeLink(id)}>Remove</Button>
+            </div>
 
-        <div className="relative mt-4">
-          <Input
-            label={'Link'}
-            classname="pl-7.5"
-            placeholder={'e.g. https://yourlink.com'}
-            onChange={(e) => updateLink(id, url, e.target.value)}
-          />
-          <FaLink className="absolute top-[42px] left-[7px]" />
+            <div className="relative mx-auto mt-5">
+                <label className="block text-sm mb-2 text-white">
+                    Platform
+                </label>
+
+                <div
+                    className="w-full border border-gray-500 p-2 rounded bg-black text-white cursor-pointer flex justify-between items-center"
+                    onClick={() => setIsOpen(!isOpen)}>
+                    {selectedOption ? (
+                        <div className="flex gap-2 items-center">
+                            {selectedOption.icon}
+                            <span>{selectedOption.name}</span>
+                        </div>
+                    ) : (
+                        <span className="text-gray-400">Choose a platform</span>
+                    )}
+                    <span className="ml-auto">&#9662;</span>
+                </div>
+                {errors?.platform && (
+                    <p className="text-red-500 text-sm mt-1">Platform is required</p>
+                )}
+
+                {isOpen && (
+                    <div className="absolute z-10 w-full bg-black border border-gray-600 mt-1 rounded shadow-lg max-h-60 overflow-y-auto">
+                        {linkOptions.map((option) => (
+                            <div key={option.value || 'no-value'}>
+                                <div
+                                    className="flex items-center gap-2 px-4 py-2 hover:bg-gray-700 cursor-pointer"
+                                    onClick={() => {
+                                        if (!option.value) return;
+                                        setSelected(option.value);
+                                        updateLink(
+                                            id,
+                                            'platform',
+                                            option.value,
+                                            option.icon.type.name
+                                        );
+                                        setIsOpen(false);
+                                    }}>
+                                    {option.icon}
+                                    {option.name}
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                )}
+
+                <div className="relative mt-4">
+                    <Input
+                        label={'Link'}
+                        classname="pl-7.5"
+                        placeholder={'e.g. https://yourlink.com'}
+                        // {...register('url', {
+                        //     required: 'Link is required',
+                        //     pattern: {
+                        //         value: /^(ftp|http|https):\/\/[^ "]+$/,
+                        //         message: 'Invalid URL format',
+                        //     },
+                        // })}
+                        onChange={(e) => {
+                            updateLink(id, 'url', e.target.value);
+                        }}
+                    />
+                    {errors?.url && (
+                        <p className="text-red-500 text-sm mt-1">
+                        {errors.message || 'Invalid URL format'}
+                    </p>
+                    )}
+                    <FaLink className="absolute top-[42px] left-[7px]" />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Links;
